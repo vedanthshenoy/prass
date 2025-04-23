5b26e4cebbffea838b2dc55c410262f259ca3ad8
vedanthshenoy <vedantshenoy1@gmail.com>
2025-04-23 16:36:45 +0530

    feat: server for add and multiply functions

---
+++ agentdry/servers/mcp_trial_server.py
@@ -0,0 +1,21 @@
+from mcp.server.fastmcp import FastMCP
+from pydantic import BaseModel, Field
+
+mcp = FastMCP("Math")
+
+
+@mcp.tool()
+def add(a: int, b: int) -> int:
+    """Add two numbers"""
+    return a + b
+
+@mcp.tool()
+def multiply(a: int, b: int) -> int:
+    """Multiply two numbers"""
+    return a * b
+
+
+
+if __name__ == "__main__":
+    # Initialize and run the server
+    mcp.run(transport='sse')
