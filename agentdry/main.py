bc4c210bc2eea9d1ddf8757736c9b0aece2b7414
vedanthshenoy <vedantshenoy1@gmail.com>
2025-06-05 01:45:48 +0530

    chore: Updated user end messages

--- agentdry/main.py
+++ agentdry/main.py
@@ -1,11 +1,10 @@
 from utils.append_to_server import create_tool_from_user_input, append_code_to_server_file
 
-def create_and_update_tool(query: str, server_file_path : str = r"C:\prass\agentdry\servers\mcp_trial_server.py"):
-    print(query)
+def create_and_update_tool(query: str, server_file_path : str = r"C:\prass\agentdry\servers\mcp_server_autorestart.py"):
     code_string = create_tool_from_user_input(query)
     append_code_to_server_file(code_string, server_file_path)
     print("Server Updated !!!")
-    print("Ask the query again to obtain the answer.") # will route query back in future versions rather than user asking again
+    print("Please Wait 🙏") # will route query back in future versions rather than user asking again
     
 if __name__ == "__main__":
     create_and_update_tool("How to fix langchain sandbox issue?")
